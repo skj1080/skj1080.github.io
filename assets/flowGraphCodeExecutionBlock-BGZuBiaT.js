9c2477d2556b47e9355534cd131adcfc6c6925ed
skj1080 <1080skj@gmail.com>
2025-05-23 12:47:03 +0900

    update assets

---
+++ assets/flowGraphCodeExecutionBlock-BGZuBiaT.js
@@ -0,0 +1,27 @@
+import { w as i, x as e, __tla as __tla_0 } from "./index-C4YyaH4g.js";
+let o;
+let __tla = Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  o = class extends i {
+    constructor(t) {
+      super(t), this.config = t, this.executionFunction = this.registerDataInput("function", e), this.value = this.registerDataInput("value", e), this.result = this.registerDataOutput("result", e);
+    }
+    _updateOutputs(t) {
+      const s = this.executionFunction.getValue(t), u = this.value.getValue(t);
+      s && this.result.setValue(s(u, t), t);
+    }
+    getClassName() {
+      return "FlowGraphCodeExecutionBlock";
+    }
+  };
+});
+export {
+  o as FlowGraphCodeExecutionBlock,
+  __tla
+};
---
+++ assets/tgaTextureLoader-DW-XLwCn.js
@@ -0,0 +1,29 @@
+import { g as r, h as n, __tla as __tla_0 } from "./index-C4YyaH4g.js";
+let d;
+let __tla = Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  d = class {
+    constructor() {
+      this.supportCascades = false;
+    }
+    loadCubeData() {
+      throw ".env not supported in Cube.";
+    }
+    loadData(e, t, o) {
+      const s = new Uint8Array(e.buffer, e.byteOffset, e.byteLength), a = r(s);
+      o(a.width, a.height, t.generateMipMaps, false, () => {
+        n(t, s);
+      });
+    }
+  };
+});
+export {
+  d as _TGATextureLoader,
+  __tla
+};
